b67481e20587d76fe36323f2b97099c0b04d45c0
Hudolf Hess <hudolf@gmail.com>
2017-08-04 20:15:17 -0300

    Test - should init component with default value prop on search state

--- src/__tests__/components/SearchBar/SearchBar.spec.js
+++ src/__tests__/components/SearchBar/SearchBar.spec.js
@@ -1,9 +1,17 @@
 import React from 'react'
-import { render } from 'enzyme'
+import { render, shallow } from 'enzyme'
 import SearchBar from '../../../components/SearchBar/SearchBar'
 
 describe('SearchBar.jsx', () => {
     it('Should render SearchBar without errors', () => {
         render(<SearchBar onChange={() => {}} defaultValue='default value' />)
+    })
+
+    it('Should init component with defaultValue prop on search state', () => {
+        const defaultValueProperty = 'default value'
+
+        const wrapper = shallow(<SearchBar onChange={() => {}} defaultValue={defaultValueProperty} />)
+
+        expect(defaultValueProperty).toEqual(wrapper.state().search)
     })
 })
