d302a9b980333216f2d44b5e7f976bd976d85278
Hudolf Hess <hudolf@gmail.com>
2017-08-05 10:59:18 -0300

    Last tests of SearchBar

--- src/__tests__/components/SearchBar/SearchBar.spec.js
+++ src/__tests__/components/SearchBar/SearchBar.spec.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { render, shallow } from 'enzyme'
 import SearchBar from '../../../components/SearchBar/SearchBar'
 
+jest.useFakeTimers()
+
 describe('SearchBar.jsx', () => {
     it('Should render SearchBar without errors', () => {
         render(<SearchBar onChange={() => {}} defaultValue='default value' />)
@@ -23,5 +25,32 @@ describe('SearchBar.jsx', () => {
         wrapper.setState({ search: changingValue })
 
         expect(changingValue).toEqual(wrapper.find('input').props().value)
+    })
+
+    it('Should call onChange prop when onChange from input is called', () => {
+        const onChangeMock = jest.fn()
+        const valueOnChange = 'changing value'
+
+        const wrapper = shallow(<SearchBar onChange={onChangeMock} defaultValue='any value' />)
+        wrapper.find('input').simulate('change', { target: { value: valueOnChange }})
+        jest.runOnlyPendingTimers()
+
+        expect(valueOnChange).toEqual(onChangeMock.mock.calls[0][0])
+    })
+
+    it('Should call only one time onChange with the last value prop when input is changed multiples time in sequence', () => {
+        const onChangeMock = jest.fn()
+
+        const wrapper = shallow(<SearchBar onChange={onChangeMock} defaultValue='any value' />)
+        wrapper.find('input').simulate('change', { target: { value: 's' }})
+        wrapper.find('input').simulate('change', { target: { value: 'sp' }})
+        wrapper.find('input').simulate('change', { target: { value: 'spi' }})
+        wrapper.find('input').simulate('change', { target: { value: 'spid' }})
+        wrapper.find('input').simulate('change', { target: { value: 'spide' }})
+        wrapper.find('input').simulate('change', { target: { value: 'spider' }})
+        jest.runOnlyPendingTimers()
+
+        expect(1).toEqual(onChangeMock.mock.calls.length)
+        expect('spider').toEqual(onChangeMock.mock.calls[0][0])
     })
 })
